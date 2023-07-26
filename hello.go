ffcc23551786ed92f413e546ab51f46cd7fab45f
Mark Teekens <mark@rubyfinance.nl>
2023-07-26 22:19:27 +0200

    refactor: removed unneccesary code

--- hello.go
+++ hello.go
@@ -1,7 +1,3 @@
-// ⚡️ Fiber is an Express inspired web framework written in Go with ☕️
-// 🤖 Github Repository: https://github.com/gofiber/fiber
-// 📌 API Documentation: https://docs.gofiber.io
-
 package main
 
 import (
@@ -12,7 +8,6 @@ import (
 )
 
 func main() {
-	// Fiber instance
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
@@ -20,23 +15,20 @@ func main() {
 		AllowHeaders:  "Origin, Content-Type, Accept",
 	}))
 
-	// Routes
 	app.Get("/", hello)
 
-	// Get the PORT from heroku env
 	port := os.Getenv("PORT")
 
-	// Verify if heroku provided the port or not
 	if os.Getenv("PORT") == "" {
 		port = "3001"
 	}
 
-	// Start server on http://${heroku-url}:${port}
 	log.Fatal(app.Listen("0.0.0.0:" + port))
 }
 
 
-// Handler
 func hello(c *fiber.Ctx) error {
-	return c.SendString("Hello, World 👋!")
+	message := "World"
+	
+	return c.SendString("Hello" + message + " 👋!")
 }
