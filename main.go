c3c4326ba79799d85857f6ee8cf1f796765aa496
Mark Teekens <mark@rubyfinance.nl>
2023-08-06 01:50:02 +0200

    feat: splitted files, ad concurrency for multiple db queries and return to json format

--- main.go
+++ main.go
@@ -1,39 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-
-	// "gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
-
-type Products struct {
-	gorm.Model
-	id         string
-	name       string
-	collection string
-	image      string
-	created_at string
-	updated_at string
-	deleted_at string
-	code       string
-	price      string
-}
-type Person struct {
-	Email   string `json:"email"`
-	Balance string `json:"balance"`
-}
-type Candidate struct {
-	Name       string   `json:"name"`
-	Interests  []string `json:"interests"`
-	Language   string   `json:"language"`
-	Experience bool     `json:"experience"`
-}
 
 func main() {
 
@@ -44,9 +17,7 @@ func main() {
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
-	app.Get("/api/message", generateMessage)
-
-	app.Get("/api/concurrency", queryMessage)
+	app.Get("/api/get-data", generateMessage)
 
 	port := os.Getenv("PORT")
 
@@ -58,113 +29,30 @@ func main() {
 }
 
 func generateMessage(c *fiber.Ctx) error {
-	// db, err := gorm.Open(mysql.Open("ice7w3ai5sg3qkntzjj7:pscale_pw_CPeqjtaSekzjVPS9b1AIjLDyrSpQWxZ3N7pIWQtUUdO@tcp(aws.connect.psdb.cloud)/test?tls=true"), &gorm.Config{})
+	// Create channels to receive the results from Goroutines
+	peopleChan := make(chan []map[string]interface{})
+	ordersChan := make(chan []map[string]interface{})
 
-	// if err != nil {
-	// 	panic("Failed to connect database")
-	// }
+	// Start Goroutines to fetch data concurrently
+	go func() {
+		people := querydb("people")
+		peopleChan <- people
+	}()
+	go func() {
+		orders := querydb("orders")
+		ordersChan <- orders
+	}()
 
-	// result := map[string]interface{}{}
-	// db.Model(&Products{}).First(&result)
+	// Wait for the results from Goroutines
+	people := <-peopleChan
+	orders := <-ordersChan
 
-	candidates := []Candidate{
-		{
-			Name:       "ravi",
-			Interests:  []string{"art", "coding", "music", "travel"},
-			Language:   "golang",
-			Experience: false,
-		},
-		{
-			Name:       "mark",
-			Interests:  []string{"art", "coding", "music", "travel"},
-			Language:   "typescript",
-			Experience: false,
-		},
-		{
-			Name:       "xavi",
-			Interests:  []string{"art", "coding", "music", "travel"},
-			Language:   "typescript",
-			Experience: false,
-		},
-		{
-			Name:       "roger",
-			Interests:  []string{"art", "coding", "music", "travel"},
-			Language:   "typescript",
-			Experience: false,
-		},
-		{
-			Name:       "vascos",
-			Interests:  []string{"art", "coding", "music", "travel"},
-			Language:   "typescript",
-			Experience: false,
-		},
-		{
-			Name:       "ravi",
-			Interests:  []string{"art", "coding", "music", "travel"},
-			Language:   "golang",
-			Experience: false,
-		},
-		{
-			Name:       "mark",
-			Interests:  []string{"art", "coding", "music", "travel"},
-			Language:   "typescript",
-			Experience: false,
-		},
-		{
-			Name:       "xavi",
-			Interests:  []string{"art", "coding", "music", "travel"},
-			Language:   "typescript",
-			Experience: false,
-		},
-		{
-			Name:       "roger",
-			Interests:  []string{"art", "coding", "music", "travel"},
-			Language:   "typescript",
-			Experience: false,
-		},
-		{
-			Name:       "vascos",
-			Interests:  []string{"art", "coding", "music", "travel"},
-			Language:   "typescript",
-			Experience: false,
-		},
-	}
+	// Close the channels
+	close(peopleChan)
+	close(ordersChan)
 
 	return c.JSON(fiber.Map{
-		"candidates": candidates})
-}
-
-func queryMessage(c *fiber.Ctx) error {
-	convertedEmail := make(chan string)
-	convertedBalance := make(chan string)
-
-	go func() {
-		result := performTask()
-		convertedEmail <- result
-	}()
-
-	go func() {
-		result := performTaskTwo()
-		convertedBalance <- result
-	}()
-
-	user := Person{
-		Email:   <-convertedEmail,
-		Balance: <-convertedBalance,
-	}
-
-	u, err := json.Marshal(user)
-
-	if err != nil {
-		panic(err)
-	}
-	return c.SendString(string(u))
-}
-
-func performTask() string {
-	return "mark.teekens@outlook.com"
-}
-
-func performTaskTwo() string {
-	return "10"
+		"people": people,
+		"orders": orders,
+	})
 }
